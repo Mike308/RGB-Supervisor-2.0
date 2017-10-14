20c4d902de947fd588e8fb06687184cfdd4f84be
Majkel44 <m.werner9205@gmail.com>
2017-10-14 18:28:23 +0200

    - added loading current list of coms - passing com list to qml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QSerialPortInfo>
+
 #include "rgbcontroller.h"
 
 
@@ -9,11 +11,24 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     RGBController rgbController;
+    QSerialPortInfo serialPortInfo;
+    QList<QSerialPortInfo> serialPortInfoList = serialPortInfo.availablePorts();
+    QStringList portsName;
+
+    foreach (QSerialPortInfo port, serialPortInfoList) {
+
+         portsName.append(port.portName());
+
+    }
+
+
+
 
 
     QQmlApplicationEngine engine;
     QQmlContext *appContext = engine.rootContext();
     appContext->setContextProperty("rgbController",&rgbController);
+    appContext->setContextProperty("portsNameModel",QVariant::fromValue(portsName));
 
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
