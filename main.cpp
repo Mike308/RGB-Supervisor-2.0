8ce4319f9ec50aa4adfe6950dbc2d277cfd47814
Majkel44 <m.werner9205@gmail.com>
2017-10-14 14:09:31 +0200

    - added object rgbController - added appContext - added setting context property

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "rgbcontroller.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+    RGBController rgbController;
+
 
     QQmlApplicationEngine engine;
+    QQmlContext *appContext = engine.rootContext();
+    appContext->setContextProperty("rgbController",&rgbController);
+
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
     return app.exec();
