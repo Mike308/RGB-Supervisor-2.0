564f694723903fac17da1436cca418fab12b9e72
Majkel44 <m.werner9205@gmail.com>
2017-10-14 12:47:07 +0200

    - implemented slot connectToDevice

---
+++ rgbcontroller.cpp
@@ -0,0 +1,23 @@
+#include "rgbcontroller.h"
+
+RGBController::RGBController()
+{
+
+}
+
+
+void RGBController::connectToDevice(QString comName, int baud){
+
+    if (!this->isOpen()){
+
+        this->setPortName(comName);
+        this->setBaudRate(baud);
+        this->setFlowControl(QSerialPort::NoFlowControl);
+        this->setDataBits(QSerialPort::Data8);
+        this->setStopBits(QSerialPort::OneStop);
+        this->open(QIODevice::ReadWrite);
+
+
+    }
+
+}
