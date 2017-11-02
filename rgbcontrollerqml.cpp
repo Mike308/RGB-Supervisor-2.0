5525fe95535425f9f7913a3d7588ab750de68a1e
Mikołaj Werner <m.werner9205@gmail.com>
2017-11-02 17:38:49 +0100

    - added in constructor creating RGBConctroller object and timer object - in destructor added quting and waiting rgbController thread - in slot connectToDevice added connection between setParameterSignal and setConnectionParameters - in slot connectToDevice added emitting signal to setup connection parameter - in slot connectToDevice created rgbControllerThread - in slot connectToDevice moved rgbController to rgbControllerThread Thread - in slot connectToDevice added connection between strated signal of rgbControllerThread and slot connectToDevice in rgbController - in slot connectToDevice added connection between finished signal of rgbControllerThread and slot deleteLater - in slot connectToDevice added connection between slot in rgbController and signal in rgbControllerQML - in slot connectToDevice added starting thread - in slot setAnimation added emitng signal to setup animation - in slot setHSV added emiting signal to setup hsv color - in slot setRGB added emiting signal to set rgb color - in slot disconnectFromDevice added emiting signal to disconnection and send request to interrupt rgbControllerThread - in slot getConnecting added emiting information about connecting to device - in slot getConnectingFinish added emiting information about finished connecting to device

---
+++ rgbcontrollerqml.cpp
@@ -0,0 +1,95 @@
+#include "rgbcontrollerqml.h"
+
+
+RGBControllerQML::RGBControllerQML()
+{
+    rgbController = new RGBController();
+
+    timer = new QTimer();
+
+
+
+}
+
+RGBControllerQML::~RGBControllerQML(){
+
+    rgbControllerThread->quit();
+    rgbControllerThread->wait();
+
+
+
+}
+
+void RGBControllerQML::connectToDevice(QString comName, int baud){
+
+    connect(this,SIGNAL(setParameterSignal(QString,int)),rgbController,SLOT(setConnectionParameters(QString,int)));
+    emit setParameterSignal(comName,baud);
+
+    rgbControllerThread = new QThread();
+    rgbController->moveToThread(rgbControllerThread);
+    connect(rgbControllerThread,SIGNAL(started()),rgbController,SLOT(connectToDevice()));
+    connect(rgbControllerThread,SIGNAL(finished()),rgbController,SLOT(deleteLater()));
+    connect(rgbController,SIGNAL(isConnecting()),this,SLOT(getConnecting()));
+    connect(rgbController,SIGNAL(isConnectingFinish()),this,SLOT(getConnectingFinish()));
+
+    connect(this,SIGNAL(disconnectSignal()),rgbController,SLOT(disconnectFromDevice()));
+    connect(this,SIGNAL(setRGBSignal(int,int,int)),rgbController,SLOT(setRGB(int,int,int)));
+    connect(this,SIGNAL(setHSVSignal(int,int,int)),rgbController,SLOT(setHSV(int,int,int)));
+    connect(this,SIGNAL(setAnimationSignal(int,int,int)),rgbController,SLOT(setAnimation(int,int,int)));
+    connect(this,SIGNAL(getConnectionStatusSignal()),rgbController,SLOT(getConnectionStatus()));
+
+
+    rgbControllerThread->start();
+
+
+}
+
+void RGBControllerQML::setAnimation(int type, int speed, int step){
+
+    emit setAnimationSignal(type,speed,step);
+}
+
+void RGBControllerQML::setHSV(int h, int s, int v){
+
+    emit setHSVSignal(h,s,v);
+}
+
+void RGBControllerQML::setRGB(int r, int g, int b){
+
+    emit setRGBSignal(r,g,b);
+}
+
+//void RGBControllerQML::sendGetTemperatureRequest(){
+
+//    //rgbController->sendGetTemperatureRequest();
+//}
+
+bool RGBControllerQML::getConnectionStatus(){
+
+    return rgbController->getConnectionStatus();
+
+}
+
+void RGBControllerQML::disconnectFromDevice(){
+
+
+    emit disconnectSignal();
+    rgbControllerThread->requestInterruption();
+
+
+}
+
+void RGBControllerQML::getConnecting(){
+
+    emit qmlConnecting();
+}
+
+void RGBControllerQML::getConnectingFinish(){
+
+    emit qmlConnectingFinish();
+}
+
+
+
+
+
